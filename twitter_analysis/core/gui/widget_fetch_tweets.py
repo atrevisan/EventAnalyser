ed020420cabea119e24b83e2335c73f935e664ba
atrevisan <allan.trvsn@gmail.com>
2015-04-10 11:04:18 -0300

    new disposal of elements

--- twitter_analysis/core/gui/widget_fetch_tweets.py
+++ twitter_analysis/core/gui/widget_fetch_tweets.py
@@ -20,12 +20,7 @@ import os
 
 class WidgetFetchTweets(QWidget, Ui_widget_fetch_tweets):
     """This widget contain gui elements for fetching tweets that correspond to a given search querry.
-            
-    Parameters
-    ----------
-    main_window : MainWindow
-        A reference for the application main window so it can change the status bar.
-
+ 
     Atributes
     -------
     _active : Boolean
@@ -35,26 +30,68 @@ class WidgetFetchTweets(QWidget, Ui_widget_fetch_tweets):
         map hashtags to frequencies.
 
     tweets : list of tuples
-        store tweets in the form (created_at, retweet_count, tweet_text).
+        store tweets in the form (created_at, retweet_count, tweet_text, latitude, longitude).
     """
-    def __init__(self, main_window):
+    def __init__(self):
 
         QWidget.__init__(self)
  
-        self.main_window = main_window
-
         # set up User Interface (widgets, layout...)
         self.setupUi(self)
 
-        # for some reason not loading icons correctly inside designer
-        self.button_save_tweets.setIcon(QtGui.QIcon(QtGui.QPixmap(os.getcwd() + r"\core\gui\assets\save.png")))
-        self.button_fetch_tweets.setIcon(QtGui.QIcon(QtGui.QPixmap(os.getcwd() + r"\core\gui\assets\mg.png")))
-        self.button_stop_fetching.setIcon(QtGui.QIcon(QtGui.QPixmap(os.getcwd() + r"\core\gui\assets\stop.png")))
+        root_directory = os.getcwd()
+        root_directory = root_directory.replace("\\", "/")
+
+        stylesheet = """QPushButton#button_save_tweets {
+            
+                            background-color: transparent;
+                            background-image: url('%s/core/gui/assets/save.png');
+                            background-repeat: no-repeat;
+                        }
+        
+                        QPushButton#button_save_tweets:hover {
+                            background-color: transparent;
+                            background-image: url('%s/core/gui/assets/save_2.png');
+                            background-repeat: no-repeat;
+                        }
+            
+                        QPushButton#button_save_tweets:pressed {
+                            background-color: transparent;
+                            background-image: url('%s/core/gui/assets/save.png');
+                            background-repeat: no-repeat;
+                        }
+        """ %(root_directory, root_directory, root_directory)
+
+        self.button_save_tweets.setStyleSheet(stylesheet)
+
+        stylesheet = """QPushButton#button_fetch_tweets {
+
+	                        background-color: rgb(255, 255, 127);
+	                        border-radius: 7px; 
+	                        border: 2px solid gray;
+                            background-image: url('%s/core/gui/assets/mg2.png');
+                            background-repeat: no-repeat; 
+                            background-position: center;
+                    }
+
+                    QPushButton#button_fetch_tweets:hover {
+	                        background-color: rgb(227, 255, 82);
+	                        border-style: inset;
+
+                    }
+ 
+                    QPushButton#button_fetch_tweets:pressed {
+	                        background-color: rgb(255, 255, 127);
+	                        border-style: inset;
+                    }
+                    """%root_directory
+
+        self.button_fetch_tweets.setStyleSheet(stylesheet)
 
         # custom event handling
         self.button_fetch_tweets.clicked.connect(self.fetch_tweets)
-        self.button_stop_fetching.clicked.connect(self.stop_fetching)
         self.button_save_tweets.clicked.connect(self.save_tweets)
+        self.button_append_to_file.clicked.connect(self.append_to_file)
       
         self._active = False    
 
@@ -66,9 +103,6 @@ class WidgetFetchTweets(QWidget, Ui_widget_fetch_tweets):
 
         if not self._active:
             self._active = True
-
-            self.main_window.statusBar().setStyleSheet("")
-            self.main_window.statusBar().showMessage("")
 
             QtCore.QTimer.singleShot(0, self.ask_twitter)
         else:
@@ -153,6 +187,17 @@ class WidgetFetchTweets(QWidget, Ui_widget_fetch_tweets):
                 # STEP 2: Save the returned tweets
                 for result in results['statuses']:
 
+                    latitude = 'na'
+                    longitude = 'na'
+
+                    if not result['geo'] is None:
+                        
+                        coordinates = result['geo']['coordinates']
+
+                        if coordinates[1] != 0.0:
+                            latitude = coordinates[0]
+                            longitude = coordinates[1]
+
                     tweet_text = result['text']
                     hashtags_list = re.findall(r"#([^\s]+)", tweet_text)
 
@@ -166,7 +211,7 @@ class WidgetFetchTweets(QWidget, Ui_widget_fetch_tweets):
                     if not tweet_text in tweet_text_list:
                         
                         tweet_text_list.append(tweet_text)
-                        tweet = (created_at, retweet_count, tweet_text)
+                        tweet = (created_at, retweet_count, tweet_text, latitude, longitude)
                         self.tweets.append(tweet)
 
                         tweet_count += 1
@@ -193,30 +238,21 @@ class WidgetFetchTweets(QWidget, Ui_widget_fetch_tweets):
                 i += 1
 
         finally:
-            print ("Finish fetching")
+            print ("Finished fetching")
            
-    def stop_fetching(self):
-        """It stops the fetching process in response for a click event and updates the status bar.
- 
-        Parameters
-        ----------
-        self
-        """
-        if self._active:
-            self._active = False
-            self.main_window.statusBar().setStyleSheet("QStatusBar{padding-left:8px;background:rgba(255,0,0,255);color:black;font-weight:bold;}")
-            self.main_window.statusBar().showMessage("Finished fetching tweets")
-
     def save_tweets(self):
-        """It saves the fetched tweets to the user chosen location in response for a click event.
+        """It saves the fetched tweets to the user chosen directory location in response for a click event.
  
-        It stores the retrived tweets to a csv file in the format: (date, retweets_count, tweet_text). 
+        It stores the retrived tweets to a csv file in the format: (date, retweet_count, tweet_text, latitutude, longitude). 
         It also stores a file containing hashtag counts in a dict format.
 
         Parameters
         ----------
         self
         """
+        
+        if self._active:
+            self._active = False
              
         file_name = QtGui.QFileDialog.getSaveFileName(self, "Save data", os.getcwd() + "\\tweets\\", "*.csv")
 
@@ -228,8 +264,48 @@ class WidgetFetchTweets(QWidget, Ui_widget_fetch_tweets):
         csv_writer = csv.writer(csv_file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
 
         for tweet in self.tweets:
-            csv_writer.writerow(['|'+str(tweet[0])+'|', '|'+str(tweet[1])+'|', '|'+tweet[2]+'|'])
+            csv_writer.writerow(['|'+str(tweet[0])+'|', '|'+str(tweet[1])+'|', '|'+tweet[2]+'|', '|'+str(tweet[3])+'|', '|'+str(tweet[4])+'|'])
         csv_file.close()
            
         with open(file_name + "_hashtags.txt", 'wb') as handle:
             pickle.dump(self.hashtags, handle)
+
+    def append_to_file(self):
+        """It appends the fetched tweets to the user file of choise.
+ 
+        It stores the retrived tweets to a csv file in the format: (date, retweet_count, tweet_text, latitutude, longitude). 
+        It also stores a file containing hashtag counts in a dict format.
+
+        Parameters
+        ----------
+        self
+        """
+        
+        if self._active:
+            self._active = False
+             
+        file_name = QtGui.QFileDialog.getSaveFileName(self, "Save data", os.getcwd() + "\\tweets\\", "*.csv")
+
+        csv_file = open(file_name, 'a', newline='', encoding="utf-8")
+        csv_writer = csv.writer(csv_file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+
+        for tweet in self.tweets:
+            csv_writer.writerow(['|'+str(tweet[0])+'|', '|'+str(tweet[1])+'|', '|'+tweet[2]+'|', '|'+str(tweet[3])+'|', '|'+str(tweet[4])+'|'])
+        csv_file.close()
+
+        # loading hashtags
+        with open(file_name[:-4] + "_hashtags.txt", 'rb') as handle:
+            hashtags = pickle.loads(handle.read())
+           
+
+        # updating hashtags dict
+        for hashtag in self.hashtags.items():
+
+            if hashtag in hashtags:
+                hashtags[hashtag] += 1
+            else:
+                hashtags[hashtag] = 1
+
+        with open(file_name[:-4] + "_hashtags.txt", 'wb') as handle:
+            pickle.dump(hashtags, handle)
+            
