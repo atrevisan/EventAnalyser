f2938a99ee67f8a9655783bf852f202385aad948
atrevisan <allan.trvsn@gmail.com>
2015-05-26 11:21:37 -0300

    + widget ngrams retweet info per hour

--- twitter_analysis/core/gui/widget_ngrams.py
+++ twitter_analysis/core/gui/widget_ngrams.py
@@ -87,21 +87,17 @@ class WidgetNGrams(QWidget, Ui_widget_ngrams):
 
         list_of_years = []
         list_of_months = []
-        list_of_days = []
         for tweet in self.tweets:
 
             tweet_time = tweet[1]
             tweet_year = tweet_time.split()[5]
             tweet_month = tweet_time.split()[1]
-            tweet_day = tweet_time.split()[2]
-
+            
             list_of_years.append(tweet_year)
             list_of_months.append(tweet_month)
-            list_of_days.append(tweet_day)
-
+            
         self.combo_year.addItems(list(set(list_of_years)))
         self.combo_month.addItems(list(set(list_of_months)))
-        self.combo_day.addItems(list(set(list_of_days)))
 
         self.tokenize = clustering_feature_extractor.vectorizer.build_analyzer()
 
@@ -113,11 +109,11 @@ class WidgetNGrams(QWidget, Ui_widget_ngrams):
 
         self.combo_year.activated[str].connect(self.on_activated_combo_year)
         self.combo_month.activated[str].connect(self.on_activated_combo_month)
-        #self.combo_day.activated[str].connect(self.on_activated_combo_day)
+        self.combo_day.activated[str].connect(self.on_activated_combo_day)
 
         self.button_info_per_month.clicked.connect(self.generate_info_per_month)
         self.button_info_per_day.clicked.connect(self.generate_info_per_day)
-        #self.button_info_per_hour.clicked.connect(self.generate_info_per_hour)
+        self.button_info_per_hour.clicked.connect(self.generate_info_per_hour)
 
     def on_activated_combo_ngrams(self, ngram):
         """Handle events on the combo box n-grams.
@@ -168,6 +164,87 @@ class WidgetNGrams(QWidget, Ui_widget_ngrams):
         self.combo_day.setDisabled(False)
         self.button_info_per_hour.setDisabled(False)
 
+        self.combo_day.clear()
+
+        list_of_days = []
+        for tweet in self.tweets:
+
+            tweet_time = tweet[1]
+            
+            tweet_month = tweet_time.split()[1]
+
+            if tweet_month == self.month:
+
+                tweet_day = tweet_time.split()[2]
+                list_of_days.append(tweet_day)
+                
+        self.combo_day.addItems(sorted(list(set(list_of_days))))
+
+    def on_activated_combo_day(self, day):
+        """Handle event on the combo box day.
+        
+        Sets the chosen day variable. The respective seted value will
+        be used to generate the n-gram distribution troughout
+        the hours of the day.
+
+        day : str
+            The day chosen by the user in the combo box.
+        """
+
+        self.day = day
+
+    def generate_and_plot_info(self, ngram_info, x_label, y_label, title, x_ticks=[], min_x=0, max_x=0):
+        """Generate the infos for the ngram and plot it.
+        
+        The info could be the frequency, retweets, positive sentiment or negative sentiment
+        relations to the chosen ngram (namely absolute frequency, mean frequency and max frequency).
+
+        Parameters
+        ----------
+        ngram_info : list of tuples (int, int, float, int)
+            The x axis component, the total, the average and max
+            component values for the three curves.
+
+        x_label : str
+            The x axis title.    
+
+        y_label : str
+            The y axis title.
+
+        title : str
+            The plot title.
+
+        x_ticks : list of str
+            The x axis values when strings.
+
+        min_x : int
+            The smaller value in the x axis range.
+
+        max_x : int
+            The largest value in the x axis range.
+        """
+ 
+        y_total = [total for (month, total, average, max) in ngram_info]
+        x_total = [month for (month, total, average, max) in ngram_info]
+
+        y_average = [average for (month, total, average, max) in ngram_info]
+        x_average = [month for (month, total, average, max) in ngram_info]
+
+        y_max = [max for (month, total, average, max) in ngram_info]
+        x_max = [month for (month, total, average, max) in ngram_info]
+            
+        max_y = sorted ([total for (month, total, average, max) in ngram_info], key=lambda x : -x)[0]
+            
+        plot_file_name = self.clusterized_dataset_path[:-4] + "_" + self.ngram.replace(" ", "_") + ".png"
+        self.plot_generator.create_three_plots(plot_file_name, 
+                                               x_total, y_total, "total", 
+                                               x_average, y_average, "average", 
+                                               x_max, y_max, "max", 
+                                               x_label, y_label, title, 
+                                               max_y, min_x=min_x, max_x=max_x, x_ticks=x_ticks)  
+         
+        self.plot_figure(plot_file_name)
+
     def generate_info_per_month(self):
         """Generate the infos for the ngram across the months in the chosen year.
         
@@ -186,26 +263,20 @@ class WidgetNGrams(QWidget, Ui_widget_ngrams):
             ngram_info_per_month = [(x_ticks.index(month) + 1, total, average, max) for month, total, average, max in ngram_info_per_month]
             ngram_info_per_month = sorted(ngram_info_per_month, key=lambda x : x[0])
 
-            y_total = [total for (month, total, average, max) in ngram_info_per_month]
-            x_total = [month for (month, total, average, max) in ngram_info_per_month]
+            if ngram_info_per_month:
+                self.generate_and_plot_info(ngram_info_per_month, "Month", "Frequency", "N-gram frequency", x_ticks)
 
-            y_average = [average for (month, total, average, max) in ngram_info_per_month]
-            x_average = [month for (month, total, average, max) in ngram_info_per_month]
+        elif self.radio_retweets.isChecked():
 
-            y_max = [max for (month, total, average, max) in ngram_info_per_month]
-            x_max = [month for (month, total, average, max) in ngram_info_per_month]
-            
-            max_y = sorted ([total for (month, total, average, max) in ngram_info_per_month], key=lambda x : -x)[0]
-            
-            plot_file_name = self.clusterized_dataset_path[:-4] + "_" + self.ngram.replace(" ", "_") + ".png"
-            self.plot_generator.create_three_plots(plot_file_name, 
-                                                   x_total, y_total, "total", 
-                                                   x_average, y_average, "average", 
-                                                   x_max, y_max, "max", 
-                                                   "Month", "Frequency", "N-gram frequncy", 
-                                                   max_y, x_ticks=x_ticks)  
-         
-            self.plot_figure(plot_file_name)  
+            ngram_retweets_info_per_month = self.twitter_data_analyser.generate_ngram_retweet_info_per_month(self.ngram,
+                                                                                                             self.year,
+                                                                                                             self.tokenize)
+
+            ngram_retweets_info_per_month = [(x_ticks.index(month) + 1, total, average, max) for month, total, average, max in ngram_retweets_info_per_month]
+            ngram_retweets_info_per_month = sorted(ngram_retweets_info_per_month, key=lambda x : x[0])
+
+            if ngram_retweets_info_per_month:
+                self.generate_and_plot_info(ngram_retweets_info_per_month, "Month", "Frequency", "N-gram retweets", x_ticks)              
             
     def generate_info_per_day(self):
         """Generate the infos for the ngram across the days in the chosen month.
@@ -218,155 +289,74 @@ class WidgetNGrams(QWidget, Ui_widget_ngrams):
            
             ngram_info_per_day = self.twitter_data_analyser.generate_ngram_frequency_info_per_day(self.ngram, 
                                                                                                   self.month, 
+                                                                                                  self.year,
                                                                                                   self.tokenize)
 
             ngram_info_per_day = [(int(day), total, average, max) for day, total, average, max in ngram_info_per_day]
             ngram_info_per_day = sorted(ngram_info_per_day, key=lambda x : x[0])
 
-            y_total = [total for (day, total, average, max) in ngram_info_per_day]
-            x_total = [day for (day, total, average, max) in ngram_info_per_day]
+            if ngram_info_per_day:
+                self.generate_and_plot_info(ngram_info_per_day, "Day", "Frequency", "N-gram frequency", min_x=1, max_x=31)
 
-            y_average = [average for (day, total, average, max) in ngram_info_per_day]
-            x_average = [day for (day, total, average, max) in ngram_info_per_day]
+        elif self.radio_retweets.isChecked():
 
-            y_max = [max for (day, total, average, max) in ngram_info_per_day]
-            x_max = [day for (day, total, average, max) in ngram_info_per_day]
+            ngram_retweets_info_per_day = self.twitter_data_analyser.generate_ngram_retweet__info_per_day(self.ngram,
+                                                                                                          self.month,
+                                                                                                          self.year,
+                                                                                                          self.tokenize)
+
+            ngram_retweets_info_per_day = [(int(day), total, average, max) for day, total, average, max in ngram_retweets_info_per_day]
+            ngram_retweets_info_per_day = sorted(ngram_retweets_info_per_day, key=lambda x : x[0])
+
+            if ngram_retweets_info_per_day:
+                self.generate_and_plot_info(ngram_retweets_info_per_day, "Day", "Frequency", "N-gram retweets", min_x=1, max_x=31)
             
-            max_y = sorted ([total for (day, total, average, max) in ngram_info_per_day], key=lambda x : -x)[0]
-            
-            plot_file_name = self.clusterized_dataset_path[:-4] + "_" + self.ngram.replace(" ", "_") + ".png"
-            self.plot_generator.create_three_plots(plot_file_name, 
-                                                   x_total, y_total, "total", 
-                                                   x_average, y_average, "average", 
-                                                   x_max, y_max, "max", 
-                                                   "Day", "Frequency", "N-gram frequncy", 
-                                                   max_y)  
-         
-            self.plot_figure(plot_file_name)  
+    def generate_info_per_hour(self):
+        """Generate the infos for the ngram across the hours in the chosen day.
+        
+        The info could be the frequency, retweets, positive sentiment or negative sentiment
+        relations to the chosen ngram (namely absolute frequency, mean frequency and max frequency).
+        """
+
+        if self.radio_frequency.isChecked():
+           
+            ngram_info_per_hour = self.twitter_data_analyser.generate_ngram_frequency_info_per_hour(self.ngram,
+                                                                                                    self.day, 
+                                                                                                    self.month,
+                                                                                                    self.year, 
+                                                                                                    self.tokenize)
+
+            ngram_info_per_hour = [(int(hour), total, average, max) for hour, total, average, max in ngram_info_per_hour]
+            ngram_info_per_hour = sorted(ngram_info_per_hour, key=lambda x : x[0])
+
+            if ngram_info_per_hour:
+                self.generate_and_plot_info(ngram_info_per_hour, "Hour", "Frequency", "N-gram frequency", min_x=0, max_x=23)
+
+        elif self.radio_retweets.isChecked():
+
+            ngram_retweets_info_per_hour = self.twitter_data_analyser.generate_ngram_retweet_info_per_hour(self.ngram,
+                                                                                                           self.day,
+                                                                                                           self.month,
+                                                                                                           self.year,
+                                                                                                           self.tokenize)
+
+            ngram_retweets_info_per_hour = [(int(hour), total, average, max) for hour, total, average, max in ngram_retweets_info_per_hour]
+            ngram_retweets_info_per_hour = sorted(ngram_retweets_info_per_hour, key=lambda x : x[0])
+
+            if ngram_retweets_info_per_hour:
+                self.generate_and_plot_info(ngram_retweets_info_per_hour, "Hour", "Frequency", "N-gram retweets", min_x=0, max_x=23)
+
 
     def plot_figure(self, file_name):
+        """Show the generated plot in the gui.
+        
+        Parameters
+        -----------
+        file_name : str
+            The location to the figure .png file.
+        """
 
         image = QtGui.QImage(file_name)
         pp = QtGui.QPixmap.fromImage(image.scaled(self.label_graph.width(), self.label_graph.height()))
             
         self.label_graph.setPixmap(pp) 
-
-    def generate_ngram_per_hour(self, file_name, ngram):
-        """Generate the ngram and ngram per cluster hourly distribution graph.
-        
-        Parameters
-        -----------
-        file_name : string
-            The file name for the ngram .png graph that will be generated.
-
-        ngram : string
-            The ngram from which the hourly frequency distribution graph
-            will be generated
-        """
-
-        # store the frequency of the given ngram per hour
-        ngram_per_hour_frequency = {}
-        for tweet in self.tweets:
-
-            tweet_time = tweet[1]
-            tweet_text = tweet[3]
-
-            tweet_tokens = self.analyser(tweet_text)
-            hour = tweet_time.split()[3].split(":")[0]
-
-            if hour in ngram_per_hour_frequency:
-
-                # test if the ngram is in the tweet
-                if ngram in tweet_tokens:
-                    ngram_per_hour_frequency[hour] += 1
-
-            else:
-                ngram_per_hour_frequency[hour] = 0
-
-                # test if the ngram is in the tweet
-                if ngram in tweet_tokens:
-                    ngram_per_hour_frequency[hour] += 1
-
-        # Generate graph plot
-        y = [y for x, y in sorted([(int(x), y) for x, y in list(ngram_per_hour_frequency.items())], key=lambda x : x[0])]
-        x = [x for x, y in sorted([(int(x), y) for x, y in list(ngram_per_hour_frequency.items())], key=lambda x : x[0])]
-
-        max_frequency = sorted (y, key=lambda x : -x)[0]
-        #print("Max frequency: %d" %max_frequency)
-
-        plt.plot(x, y, linewidth=1.5, label=ngram)
-        plt.legend()
-        plt.axis([0, 23, 0, max_frequency])
-        plt.xlabel("Hour")
-        plt.ylabel("N-gram frequency")
-        plt.title("N-gram frequency distribution")
-
-        plt.savefig(file_name)
-        plt.close()
-
-    def generate_ngram_per_day_of_week(self, file_name, ngram):
-        """Generate the ngrams and ngrams per cluster day of week distribution graphs.
-        
-        Parameters
-        -----------
-        file_name : string
-            The base name for the ngrams .png graphs that will be generated.
-
-        ngram : string
-            The ngram from wich the day of week distribution graphs
-            will be generated
-        """
-
-        # store the frequency of the given ngram per day of week
-        ngram_per_day_of_week_frequency = {}
-        for tweet in self.tweets:
-
-            tweet_time = tweet[1]
-            tweet_text = tweet[3]
-
-            tweet_tokens = self.analyser(tweet_text)
-            day_of_week = tweet_time.split()[0]
-
-            if day_of_week in ngram_per_day_of_week_frequency:
-
-                # test if the ngram is in the tweet
-                if ngram in tweet_tokens:
-                    ngram_per_day_of_week_frequency[day_of_week] += 1
-
-            else:
-                ngram_per_day_of_week_frequency[day_of_week] = 0
-
-                # test if the ngram is in the tweet
-                if ngram in tweet_tokens:
-                    ngram_per_day_of_week_frequency[day_of_week] += 1
-
-        # Generate graph plot
-        
-        x_ticks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-
-        ngram_per_day_of_week_frequency = [(x_ticks.index(x), y) for x, y in list (ngram_per_day_of_week_frequency.items())]
-
-        ngram_per_day_of_week_frequency_ordered = sorted(ngram_per_day_of_week_frequency, 
-                                                         key=lambda x : x[0])
-
-        y_values = [y for (x, y) in ngram_per_day_of_week_frequency_ordered]
-        x_values = [x for (x, y) in ngram_per_day_of_week_frequency_ordered]
-
-        max_frequency = sorted (y_values, key=lambda x : -x)[0]
-                
-        #print("Max frequency: %d" %max_frequency)
-
-        plt.plot(x_values, y_values, linewidth=1.5, label=ngram)
-        plt.legend()
-          
-        plt.xticks(np.arange(0, 7), x_ticks)
-        plt.ylim(0, max_frequency)
-                
-        plt.xlabel("Day of week")
-        plt.ylabel("N-gram frequency")
-        plt.title("N-gram frequency distribution")
-
-        plt.savefig(file_name)
-        plt.close()
-
-    
